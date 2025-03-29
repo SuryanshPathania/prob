edc59e98b470e1fe41ea8151c80e7f32d3cc06fa
SuryanshuPathania <118669699+SuryanshPathania@users.noreply.github.com>
2025-03-30 03:22:08 +0530

    Time: 46 ms (52.01%), Space: 53.7 MB (23.37%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -3,17 +3,21 @@
  * @return {Object}
  */
 var expect = function(val) {
-    return {
+    return obj = {
         toBe(val1){
-            if(val === val1) return true
-            else throw new Error("Not Equal")
+    if (val === val1) return true
+    else {
+        throw new Error("Not Equal")
+    }   
         },
-        notToBe(val1){
-            if(val !== val1) return true
-            else throw new Error("Equal")
-        }
+       notToBe(val1){
+         if (val !== val1) return true
+    else {
+        throw new Error("Equal")
     }
-    // return obj
+
+       } 
+    }
 };
 
 /**
